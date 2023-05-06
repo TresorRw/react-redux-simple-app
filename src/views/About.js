30a510810ce63eef7c0ba35c42151a67345440d1
TresorRw <alaintresorcyusa683@gmail.com>
2023-05-06 15:28:51 +0200

    Inital Commit

---
+++ src/views/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const About = () => {
+  return (
+    <>
+        <Navbar />
+        <h1>Yes am about</h1>
+    </>
+  )
+}
+
+export default About
---
+++ src/views/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>Yes ama home</h1>
+    </>
+  )
+}
+
+export default Home
